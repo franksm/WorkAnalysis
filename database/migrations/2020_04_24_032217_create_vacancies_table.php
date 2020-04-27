3c1831c6f90ca92d937f1a67f3d5a8fe71222f24
alan.yang <alan.yang@104.com.tw>
2020-04-27 16:05:26 +0800

    add seed tool category vacancy

--- database/migrations/2020_04_24_032217_create_vacancies_table.php
+++ database/migrations/2020_04_24_032217_create_vacancies_table.php
@@ -16,15 +16,15 @@ class CreateVacanciesTable extends Migration
         Schema::create('vacancies', function (Blueprint $table) {
             $table->increments('id');
             $table->string('vacancy_name',20);//職缺名稱
-            $table->string('job_category',4);//職缺類別
+            $table->string('job_category',10);//職缺類別
             $table->string('salary',20);//薪資
-            $table->string('company_name',20);//公司名稱
-            $table->string('claim_education',4);//需求學歷
-            $table->string('claim_experience',4);//工作經歷
             $table->string('work_nature',4);//工作性質
+            $table->string('company_id',20);//公司名稱
+            $table->string('claim_education',10);//需求學歷
+            $table->string('claim_experience',4);//工作經歷
             $table->string('claim_people',4);//需求人數
-            $table->boolean('management_responsibility');//使否附管理責任
-            $table->boolean('expatriate');//是否外派
+            $table->string('management_responsibility',20);//是否附管理責任
+            $table->string('expatriate',20);//是否出差外派
             $table->string('link');//細項
             $table->timestamps();
         });
