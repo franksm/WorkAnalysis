2e1da7758bc863e6faabd7e61ac82c90dc907e12
franksm <franksmhuang@gmail.com>
2020-04-27 13:24:01 +0800

    Add M Company

---
+++ database/migrations/2020_04_27_032813_create_companies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('vacancy_category',10);
+            $table->string('capital',10);
+            $table->integer('workers');
+            $table->string('region',4);
+            $table->string('area',10);
+            $table->string('link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
