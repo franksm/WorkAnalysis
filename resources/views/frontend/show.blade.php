22da9b555239ccff915f89c0e5e67ad277669bc7
alan.yang <alan.yang@104.com.tw>
2020-04-28 16:31:49 +0800

    新增公司福利欄位

--- resources/views/frontend/show.blade.php
+++ resources/views/frontend/show.blade.php
@@ -1,46 +1,68 @@
-<script language="javascript">
-    　　function openwin() {
-    　　    window.open ("/page", "newwindow", "height=100, width=400,toolbar=no,menubar=no, scrollbars=no, resizable=no, location=no, status=no")
-    　　}
-    </script>
-<div class="tag">
-        @if(count($Vacancies)>0)
-        <div class="row">
-            <div class="col-md-12">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>id</th>
-                            <th>需求技能</th>
-                            <th>職缺種類</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($Vacancies as $Vacancy)
-                        <tr>
-                            <th>{{$Vacancy->id}}</th>
-                            <th>
-                                @foreach ($Tools[$Vacancy->id] as $Tool)
-                                    <p>{{$Tool['vacancy_tool']}},</p>
-                                @endforeach
-                            </th>
-                            <th>
-                                @foreach ($Categories[$Vacancy->id] as $Category)
-                                    <p>{{$Category['vacancy_category']}},</p>
-                                @endforeach
-                            </th>
-                            <th>
-                                {{-- <p>{{$Companies[$Vacancy->id]['welfare']}}</p> --}}
-                                <button onclick="openwin()" value="{{$Companies[$Vacancy->id]['welfare']}}">Click me</button>
-                            </th>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </div>       
-        @else
-            <p>未含有任何職缺</p>
-        @endif
-    </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.0.js" integrity="sha256-r/AaFHrszJtwpe+tHyNi/XCfMxYpbsRg2Uqn0x3s2zc=" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="tag">
+            <div class="row">
+                <div class="col-md-12">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>id</th>
+                                <th>需求技能</th>
+                                <th>職缺種類</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($Vacancies as $Vacancy)
+                            <tr>
+                                <th>{{$Vacancy->id}}</th>
+                                <th>
+                                    @foreach ($Tools[$Vacancy->id] as $Tool)
+                                        {{$Tool['vacancy_tool']}},
+                                    @endforeach
+                                </th>
+                                <th>
+                                    @foreach ($Categories[$Vacancy->id] as $Category)
+                                        {{$Category['vacancy_category']}},
+                                    @endforeach
+                                </th>
+                                <th>
+                                    <div class="modal fade" id="a{{$Vacancy->id}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header text-center">
+                                                    <h4 class="modal-title w-100 font-weight-bold">公司福利</h4>
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div>
+                                                    <p>{{$Companies[$Vacancy->id]['welfare']}}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="text-center">
+                                        <a href="" class="btn btn-default btn-rounded mb-4" data-toggle="modal" data-target="#a{{$Vacancy->id}}">公司福利</a>
+                                    </div>
+                                </th>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>       
+        </div>
+        
+</body>
+</html>
