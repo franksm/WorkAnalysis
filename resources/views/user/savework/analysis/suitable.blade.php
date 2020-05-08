be963f3006e352615612f55b8b289b7110e74825
franksm <franksmhuang@gmail.com>
2020-05-08 09:52:48 +0800

    修正navbar BUG

--- resources/views/user/savework/analysis/suitable.blade.php
+++ resources/views/user/savework/analysis/suitable.blade.php
@@ -1,5 +1,5 @@
 @extends('user.savework.analysis.layouts.master')
-@section('nav_list', 'active')
+@section('nav_suitable', 'active')
 @section('content')
 <link rel="stylesheet" href="https://www.104.com.tw/jobs/apply/static/css/app.min.css?id=7bc7c107a1569c7cfad5">
 <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js"></script>
@@ -23,8 +23,32 @@
                     label: '比較2',
                     data: [30,20,10],
                     type:'bar',
-                    backgroundColor: 'rgba(255, 255, 64, 0.2)',
-                    borderColor: 'rgba(255, 159, 64, 1)',
+                    backgroundColor: 'rgba(33, 122, 64, 0.2)',
+                    borderColor: 'rgba(33, 122, 64, 1)',
+                    borderWidth: 1,
+                },
+                {
+                    label: '比較3',
+                    data: [13,21,17],
+                    type:'bar',
+                    backgroundColor: 'rgba(99, 33, 22, 0.2)',
+                    borderColor: 'rgba(99, 33, 22, 1)',
+                    borderWidth: 1,
+                },
+                {
+                    label: '比較4',
+                    data: [22,12,1],
+                    type:'bar',
+                    backgroundColor: 'rgba(255, 66, 64, 0.2)',
+                    borderColor: 'rgba(255, 66, 64, 1)',
+                    borderWidth: 1,
+                },
+                {
+                    label: '綜合',
+                    data: [44,23,11],
+                    type:'bar',
+                    backgroundColor: 'rgba(33, 44, 122, 0.2)',
+                    borderColor: 'rgba(33, 44, 122, 1)',
                     borderWidth: 1,
                 }]
             },
@@ -33,7 +57,7 @@
                     yAxes: [{
                         id: 'y-axis-1', 
                         position:'left',
-                        ticks: {min: 0},
+                        ticks: {min: 0,max: 100},
                         scaleLabel: {
                             display: true,
                             labelString: '分數'
